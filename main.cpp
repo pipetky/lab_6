33270cd66a17859cabf9a887d41d39a5c1fe44c1
pipetky <odmin@p-c.ru>
2018-12-13 17:42:17 +0300

    last

--- main.cpp
+++ main.cpp
@@ -8,32 +8,44 @@
 #include <stdlib.h>
 #include <iostream>
 #include <pthread.h>
+#include <time.h>
 using namespace std;
 
+int counter;
 
 void func(int sig)
+
 {   int a;
+    counter++;
     int status;
-    int proc =fork();
+    int proc = fork();
+    clock_t start = clock();
     if (proc == 0) {
-        cout << "__________Дочерний процесс__________" << endl << "pid = " << getpid() << endl;
-        for (int i = 0; i < 100; i++)
-        {a = 5+4;}
+        cout << endl << "__________Дочерний процесс__________" << endl << "pid = " << getpid() << endl;
+        time_t  timev;
+        time(&timev);
+        cout << "Запуск дочернего процесса " << ctime(&timev) << endl;
+        for (int i = 0; i < 10000000; i++)
+        {a = 5+4*500000;}
+
+
         exit(0);
     } else{
-        struct tms pb1, pb2;
-        times(&pb2);
+
+
         waitpid(proc, &status, WNOHANG);
 
-        times(&pb1);
-        cout << pb1.tms_stime - pb2.tms_stime << endl;
-        cout << clock() << endl;
+        clock_t stop = clock();
+        cout << "Время работы дочернего процесса с pid " << proc << " " <<(stop- start) << " такта" << endl; //Получаем время работы дочернего процесса в секундах
+
+        cout << "Время работы программы " << clock() << " тактов" << endl << "_____________________________________________________" << endl;
         return;
     }
 
 }
 
-int main() {
+int main(int argc, char * argv[]) {
+    counter = 0;
 
     struct itimerval  A,B;
     struct sigaction act;
@@ -42,13 +54,19 @@ int main() {
     sigemptyset(&act.sa_mask);
     act.sa_flags=0;
     sigaction(SIGALRM,&act,0);
+
     /* установка периода перезапуска программы*/
-    A.it_interval.tv_sec=1;
+    A.it_interval.tv_sec=atoi(argv[1]);
     A.it_interval.tv_usec=100000; // период перезапуска 100 мс
     A.it_value.tv_usec=0;
     A.it_value.tv_sec=2; // первый запуск через 2 сек
     setitimer(ITIMER_REAL, &A,  &B);
-    for (;;) pause(); /* бесконечный цикл ожидания  сигнала SIGALRM*/
+    for (;;)
+    {
+        if (counter >= atoi(argv[2]))
+            return 1;
+
+    } /* бесконечный цикл ожидания  сигнала SIGALRM*/
 
 
 
